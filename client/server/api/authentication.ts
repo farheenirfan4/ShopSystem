69422c6f9eb5e095e2c9e6cf25caa793c18f01b6
farheenirfan4 <farheen.irfan@mindstormstudios.com>
2025-08-16 21:56:33 +0500

    updates

--- client/server/api/authentication.ts
+++ client/server/api/authentication.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
   // forward it to your backend
-  const response = await $fetch('https://shop-system-5ow7.vercel.app/api/authentication', {
+  const response = await $fetch('https://shop-system-5ow7.vercel.app/authentication', {
     method: 'POST',
     body,
     headers: {
