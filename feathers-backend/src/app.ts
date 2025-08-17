347d5e64d3e66412cf5ad7ed5d2e0c2455c67714
farheenirfan4 <farheen.irfan@mindstormstudios.com>
2025-08-17 22:22:18 +0500

    Updates to include jwt

--- feathers-backend/src/app.ts
+++ feathers-backend/src/app.ts
@@ -35,6 +35,7 @@ const productionConfig = {
     service: 'users',
     secret: process.env.AUTH_SECRET || 'secret',
     authStrategies: ['jwt', 'local'],
+    path: "/api/authentication",
     jwtOptions: {
       header: { typ: 'access' },
       audience: process.env.FRONTEND_URL,
