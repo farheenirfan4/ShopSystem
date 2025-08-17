6d9da8e41fc9b409fbe5bb98b7b0867a93d70715
farheenirfan4 <farheen.irfan@mindstormstudios.com>
2025-08-17 12:29:39 +0500

    Updated code for deployment

--- feathers-backend/api/index.ts
+++
@@ -1,8 +0,0 @@
-import {app} from '../src/app'
-import type { IncomingMessage, ServerResponse } from 'http'
-
-// Vercel will pass (req, res) here
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  // Feathers Express app is also a request listener
-  return (app as any)(req, res)
-}
